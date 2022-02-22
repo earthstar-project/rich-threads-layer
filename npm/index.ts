9d176adc39de0281a56db8d4f7a5a36e288f2084
Sam Gwilym <gwilym@me.com>
2022-02-22 11:07:28 +0100

    Prep for new NPM workflow

--- npm/index.ts
+++
@@ -1,4 +0,0 @@
-// TODO: This only works while src/letterbox_layer has no relative imports.
-
-export { default } from "../src/letterbox_layer";
-export type { Post, Thread } from "../src/letterbox_layer";
